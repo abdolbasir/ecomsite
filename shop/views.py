ea7a9bf34a2375b9bfe9aaffe12557d8612a8a70
Basir Tibash <abdolbasir.tibash@gmail.com>
2025-07-27 16:41:20 +0330

    Seaech products in index.html added and the view is changed base of search item

--- shop/views.py
+++ shop/views.py
@@ -6,3 +6,10 @@ class ProductsView(ListView):
     model = Product
     template_name ="shop/index.html"
     context_object_name = "products"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        item_name = self.request.GET.get("item_name")
+        if item_name != "" and item_name is not None:
+            queryset = queryset.filter(title__icontains=item_name)
+        return queryset
