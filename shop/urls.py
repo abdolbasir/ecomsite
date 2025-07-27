f0dda6f682ecb7e65c8e3f3ded29889c61290967
Basir Tibash <abdolbasir.tibash@gmail.com>
2025-07-27 11:44:57 +0330

    The index.html view for Products list is compleated.

--- shop/urls.py
+++ shop/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductsView.as_view(), name="shop")
+    path('', views.ProductsView.as_view(), name="index")
 ]
