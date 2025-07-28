1ef3018e0fd61c6f4ad6e2d63d3849b6c45abb33
Basir Tibash <abdolbasir.tibash@gmail.com>
2025-07-28 16:09:11 +0330

    JavaScript is complated for showing all items that added to cart in Cart link

--- shop/static/shop/script.js
+++ shop/static/shop/script.js
@@ -3,6 +3,8 @@ if (localStorage.getItem('cart') == null){
     var cart = {};
 } else {
     cart = JSON.parse(localStorage.getItem('cart'));
+    document.querySelector("#cart").innerHTML = `Cart(${Object.keys(cart).length})`
+
 }
 
 $(document).on('click', '.btn-atc', function(){
@@ -13,6 +15,29 @@ $(document).on('click', '.btn-atc', function(){
         cart[item_id] = 1
     }
     localStorage.setItem('cart', JSON.stringify(cart))
-    console.log(cart)
+    document.querySelector("#cart").innerHTML = `Cart(${Object.keys(cart).length})`
  });
 
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+      popoverTriggerList.forEach(el => {
+        new bootstrap.Popover(el, {
+          trigger: 'hover',
+          html: true,
+          customClass: 'custom-dark-popover'
+        });
+      });
+cart_popover = document.querySelector('[data-bs-toggle="popover"]')
+cart_popover.setAttribute('data-bs-content', DisplayCart(cart))
+function DisplayCart(cart){
+    var cartString = "<ul class='list-group'>";
+    var carIndex = 1
+
+    Object.keys(cart).forEach(key => {
+        product_title = document.querySelector("#pt"+key).innerHTML
+        cartString += `<li  class="list-group-item d-flex justify-content-between align-items-center">${carIndex}: ${product_title}  <span class="bg-primary px-2 rounded-pill text-white" >Qty: ${cart[key]} </span> </li>` 
+        carIndex += 1
+    })   
+    cartString += "</ul>"
+    
+    return cartString
+}
